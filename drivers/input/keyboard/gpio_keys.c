f9b9804a139bf76f06105ea171443fadc2937e4f
Sean McCreary <mccreary@mcwest.org>
2020-01-08 11:10:55 -0700

    drivers: input: keyboard: Remove OEM changes
    Revert gpio_keys.c to upstream state
    
    Change-Id: I713dbf021d408e6e4c66fce97a64a98ddfc346c9

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -64,9 +64,6 @@ static struct syscore_ops gpio_keys_syscore_pm_ops;
 
 static void gpio_keys_syscore_resume(void);
 
-#define HALL_COVER_PIN (82)
-#define HALL_KEYBOARD_PIN (124)
-
 /*
  * SYSFS interface for enabling/disabling keys and switches:
  *
@@ -371,33 +368,12 @@ static void gpio_keys_gpio_report_event(struct gpio_button_data *bdata)
 	struct input_dev *input = bdata->input;
 	unsigned int type = button->type ?: EV_KEY;
 	int state;
-	
 
 	state = (__gpio_get_value(button->gpio) ? 1 : 0) ^ button->active_low;
 	if (state < 0) {
 		dev_err(input->dev.parent, "failed to get gpio state\n");
 		return;
 	}
-#if 1
-	if(button->gpio == HALL_COVER_PIN||
-		button->gpio == HALL_KEYBOARD_PIN){
-			
-		if(state){
-			input_event(input, type, button->code, 1);
-			input_sync(input);
-			udelay(20);
-			input_event(input, type, button->code, 0);
-			input_sync(input);
-		}else{
-			input_event(input, type, button->code+1, 1);
-			input_sync(input);
-			udelay(20);
-			input_event(input, type, button->code+1, 0);
-			input_sync(input);			
-		}
-		return;	
-	}
-#endif	
 
 	if (type == EV_ABS) {
 		if (state)
@@ -597,12 +573,7 @@ static int gpio_keys_setup_key(struct platform_device *pdev,
 			bdata->irq, error);
 		return error;
 	}
-	
-	if(button->type == EV_SW){
-		input_event(input, EV_SW, button->code, 1);
-		input_sync(input);
-	}
-	
+
 	return 0;
 }
 
@@ -667,7 +638,7 @@ static int gpio_keys_open(struct input_dev *input)
 	}
 
 	/* Report current state of buttons that are connected to GPIOs */
-	//gpio_keys_report_state(ddata);
+	gpio_keys_report_state(ddata);
 
 	return 0;
 }
@@ -840,12 +811,6 @@ static int gpio_keys_probe(struct platform_device *pdev)
 	input->id.vendor = 0x0001;
 	input->id.product = 0x0001;
 	input->id.version = 0x0100;
-	
-	input_set_capability(input, EV_KEY, KEY_FN_F4);
-    input_set_capability(input, EV_KEY, KEY_FN_F5);
-    input_set_capability(input, EV_KEY, KEY_FN_F6);
-    input_set_capability(input, EV_SW, SW_TABLET_MODE);
-    input_set_capability(input, EV_SW, SW_ROTATE_LOCK);
 
 	/* Enable auto repeat feature of Linux input subsystem */
 	if (pdata->rep)
@@ -1033,7 +998,7 @@ static int gpio_keys_resume(struct device *dev)
 	if (error)
 		return error;
 
-	//gpio_keys_report_state(ddata);
+	gpio_keys_report_state(ddata);
 	return 0;
 }
 
