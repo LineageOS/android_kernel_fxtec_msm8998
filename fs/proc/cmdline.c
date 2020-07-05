f0fa7aa34ff8431eb49091cbd281da2db4afb6ed
Ethan Chen <intervigil@gmail.com>
2020-07-05 01:55:24 -0700

    fs/proc: Append androidboot.mode to commandline if needed
    * The Android init binary relies on ro.boot.mode being set to "charger"
      in order to start charger mode. This device doesn't actually propagate
      the value from the bootloader, so we must parse the boot reason and
      append it in the kernel.
    
    Change-Id: I7dbc4663632667442b2e1769a5469b6bb27dfa53

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -1,11 +1,16 @@
+#include <asm/setup.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#define BOOT_REASON_PON 6
+
+static char proc_command_line[COMMAND_LINE_SIZE];
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", proc_command_line);
 	return 0;
 }
 
@@ -21,8 +26,23 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void append_boot_mode(void)
+{
+	if (boot_reason == BOOT_REASON_PON) {
+		strlcat(proc_command_line, " androidboot.mode=charger",
+				COMMAND_LINE_SIZE);
+	} else {
+		strlcat(proc_command_line, " androidboot.mode=unknown",
+				COMMAND_LINE_SIZE);
+	}
+}
+
 static int __init proc_cmdline_init(void)
 {
+	strncpy(proc_command_line, saved_command_line, COMMAND_LINE_SIZE);
+
+	append_boot_mode();
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
